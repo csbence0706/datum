6f874480f213bcb1b3216ec97c192e82699a4518
csbence0706 <csordasbence0706@gmail.com>
2024-07-02 08:18:49 +0200

    Add project files.

---
+++ datum/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace datum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEvSzam = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNap = new System.Windows.Forms.TextBox();
+            this.lblGratulacio = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNapSorszam = new System.Windows.Forms.TextBox();
+            this.txtKesobbiDatum = new System.Windows.Forms.TextBox();
+            this.txtNapSzam = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnBezar = new System.Windows.Forms.Button();
+            this.btnTorol = new System.Windows.Forms.Button();
+            this.btnKiir = new System.Windows.Forms.Button();
+            this.lblDatum = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.mskdTxtSzuldatum = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pontos dátum és idő:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ennyi éves vagy:";
+            // 
+            // txtEvSzam
+            // 
+            this.txtEvSzam.Location = new System.Drawing.Point(155, 118);
+            this.txtEvSzam.Name = "txtEvSzam";
+            this.txtEvSzam.Size = new System.Drawing.Size(104, 20);
+            this.txtEvSzam.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(334, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ilyen napon születtél:";
+            // 
+            // txtNap
+            // 
+            this.txtNap.Location = new System.Drawing.Point(446, 121);
+            this.txtNap.Name = "txtNap";
+            this.txtNap.Size = new System.Drawing.Size(104, 20);
+            this.txtNap.TabIndex = 5;
+            // 
+            // lblGratulacio
+            // 
+            this.lblGratulacio.AutoSize = true;
+            this.lblGratulacio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblGratulacio.ForeColor = System.Drawing.Color.Red;
+            this.lblGratulacio.Location = new System.Drawing.Point(226, 211);
+            this.lblGratulacio.Name = "lblGratulacio";
+            this.lblGratulacio.Size = new System.Drawing.Size(0, 20);
+            this.lblGratulacio.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 293);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Tetszőleges dátum:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(349, 293);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(126, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Ez az év ennyedik napja:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(358, 348);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(114, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "nappal későbbi dátum:";
+            // 
+            // txtNapSorszam
+            // 
+            this.txtNapSorszam.Location = new System.Drawing.Point(481, 290);
+            this.txtNapSorszam.Name = "txtNapSorszam";
+            this.txtNapSorszam.Size = new System.Drawing.Size(104, 20);
+            this.txtNapSorszam.TabIndex = 10;
+            // 
+            // txtKesobbiDatum
+            // 
+            this.txtKesobbiDatum.Location = new System.Drawing.Point(481, 345);
+            this.txtKesobbiDatum.Name = "txtKesobbiDatum";
+            this.txtKesobbiDatum.Size = new System.Drawing.Size(104, 20);
+            this.txtKesobbiDatum.TabIndex = 11;
+            // 
+            // txtNapSzam
+            // 
+            this.txtNapSzam.Location = new System.Drawing.Point(307, 345);
+            this.txtNapSzam.Name = "txtNapSzam";
+            this.txtNapSzam.Size = new System.Drawing.Size(45, 20);
+            this.txtNapSzam.TabIndex = 12;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(64, 341);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(123, 20);
+            this.dateTimePicker1.TabIndex = 13;
+            // 
+            // btnBezar
+            // 
+            this.btnBezar.Location = new System.Drawing.Point(497, 405);
+            this.btnBezar.Name = "btnBezar";
+            this.btnBezar.Size = new System.Drawing.Size(75, 23);
+            this.btnBezar.TabIndex = 14;
+            this.btnBezar.Text = "Bezár";
+            this.btnBezar.UseVisualStyleBackColor = true;
+            // 
+            // btnTorol
+            // 
+            this.btnTorol.Location = new System.Drawing.Point(287, 405);
+            this.btnTorol.Name = "btnTorol";
+            this.btnTorol.Size = new System.Drawing.Size(75, 23);
+            this.btnTorol.TabIndex = 15;
+            this.btnTorol.Text = "Töröl";
+            this.btnTorol.UseVisualStyleBackColor = true;
+            // 
+            // btnKiir
+            // 
+            this.btnKiir.Location = new System.Drawing.Point(85, 405);
+            this.btnKiir.Name = "btnKiir";
+            this.btnKiir.Size = new System.Drawing.Size(75, 23);
+            this.btnKiir.TabIndex = 16;
+            this.btnKiir.Text = "Kiír";
+            this.btnKiir.UseVisualStyleBackColor = true;
+            // 
+            // lblDatum
+            // 
+            this.lblDatum.AutoSize = true;
+            this.lblDatum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblDatum.Location = new System.Drawing.Point(155, 20);
+            this.lblDatum.Name = "lblDatum";
+            this.lblDatum.Size = new System.Drawing.Size(0, 20);
+            this.lblDatum.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(61, 75);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(84, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Születési dátum:";
+            // 
+            // mskdTxtSzuldatum
+            // 
+            this.mskdTxtSzuldatum.Location = new System.Drawing.Point(155, 75);
+            this.mskdTxtSzuldatum.Name = "mskdTxtSzuldatum";
+            this.mskdTxtSzuldatum.Size = new System.Drawing.Size(104, 20);
+            this.mskdTxtSzuldatum.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 450);
+            this.Controls.Add(this.mskdTxtSzuldatum);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblDatum);
+            this.Controls.Add(this.btnKiir);
+            this.Controls.Add(this.btnTorol);
+            this.Controls.Add(this.btnBezar);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtNapSzam);
+            this.Controls.Add(this.txtKesobbiDatum);
+            this.Controls.Add(this.txtNapSorszam);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblGratulacio);
+            this.Controls.Add(this.txtNap);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEvSzam);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEvSzam;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNap;
+        private System.Windows.Forms.Label lblGratulacio;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNapSorszam;
+        private System.Windows.Forms.TextBox txtKesobbiDatum;
+        private System.Windows.Forms.TextBox txtNapSzam;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnBezar;
+        private System.Windows.Forms.Button btnTorol;
+        private System.Windows.Forms.Button btnKiir;
+        private System.Windows.Forms.Label lblDatum;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.MaskedTextBox mskdTxtSzuldatum;
+    }
+}
+
---
+++ datum/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace datum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private DateTime ma = DateTime.Now;
+        private DateTime szulDatum;
+        private DateTime valasztottDatum;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblDatum.Text = ma.ToString("F");
+            lblGratulacio.Text = "";
+            valasztottDatum = dateTimePicker1.Value;
+            ActiveControl = mskdTxtSzuldatum;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("hu-HU");
+        }
+
+
+
+        private void lblGratulacio_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            valasztottDatum = dateTimePicker1.Value;
+            txtNapSorszam.Text = valasztottDatum.DayOfYear.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblDatum.Text = DateTime.Now.ToString("F");
+        }
+
+
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBezar_Click(object sender, EventArgs e)
+        {
+            DialogResult valasz = MessageBox.Show("Biztosan kilép?", "Megerősítés", MessageBoxButtons.YesNo);
+
+            if (valasz == DialogResult.Yes) this.Close();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mskdTxtSzuldatum_Leave_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!mskdTxtSzuldatum.MaskCompleted) throw new FormatException();
+                else
+                {
+                    szulDatum = DateTime.Parse(mskdTxtSzuldatum.Text);
+                    if (szulDatum.Month == ma.Month && szulDatum.Day == ma.Day)
+                    {
+                        lblGratulacio.Text = "Isten éltessen!";
+                    }
+                    else
+                    {
+                        lblGratulacio.Text = "Boldog hétköznapot!";
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Hibás dátum", "Hiba");
+                mskdTxtSzuldatum.Focus();
+
+            }
+        }
+
+        private void mskdTxtSzuldatum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13) mskdTxtSzuldatum_Leave_1(sender, e);
+        }
+
+        private void btnKiir_Click(object sender, EventArgs e)
+        {
+            txtEvSzam.Text = (ma.Year - szulDatum.Year).ToString();
+            txtNap.Text = szulDatum.DayOfWeek.ToString();
+            txtNapSorszam.Text = valasztottDatum.DayOfYear.ToString();
+        }
+
+        private void txtNapSzam_TextChanged(object sender, EventArgs e)
+        {
+            int hossz = txtNapSzam.Text.Length;
+            if ((hossz >= 2 && txtNapSzam.Text.ElementAt(0) == '-') || (hossz >= 1 && txtNapSzam.Text.ElementAt(0) != '-'))
+            {
+                try
+                {
+                    int nap = int.Parse(txtNapSzam.Text);
+                    txtKesobbiDatum.Text = valasztottDatum.AddDays(nap).ToString();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Nem számot írt", "Hiba");
+                }
+            }
+        }
+
+        private void btnTorol_Click(object sender, EventArgs e)
+        {
+            foreach (var item in this.Controls)
+            {
+                if (item is TextBox) ((TextBox)item).Clear();
+            }
+            lblGratulacio.Text = "";
+            mskdTxtSzuldatum.Clear();
+        }
+    }
+}
